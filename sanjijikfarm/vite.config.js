0e2dae1dbf8d56aba12e04b13671ce34f6c87384
only1Ksy <winterksy@gmail.com>
2025-09-04 21:24:03 +0900

    chore: tailwind screen set up

--- sanjijikfarm/vite.config.js
+++ sanjijikfarm/vite.config.js
@@ -1,4 +1,5 @@
 // vite.config.js
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -9,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
