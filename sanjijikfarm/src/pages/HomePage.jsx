ecff91dbeb0f5ca645eace8de988d5081acbd447
only1Ksy <winterksy@gmail.com>
2025-09-06 22:07:35 +0900

    feat: add sliders to home page

--- sanjijikfarm/src/pages/HomePage.jsx
+++ sanjijikfarm/src/pages/HomePage.jsx
@@ -1,3 +1,29 @@
+import FoodSlider from '@/components/feature/home/FoodSlider';
+import MainSlider from '@/components/feature/home/MainSlider';
+
 export default function HomePage() {
-  return <div>home</div>;
+  const TEMP_MAIN_IMAGES = [
+    { url: 'https://via.placeholder.com/600x300', main: 'Main Title', sub: 'Sub Title goes here' },
+    { url: 'https://via.placeholder.com/600x300', main: 'Main Title', sub: 'Sub Title goes here' },
+    { url: 'https://via.placeholder.com/600x300', main: 'Main Title', sub: 'Sub Title goes here' },
+  ];
+
+  const TEMP_FOOD_IMAGES = [
+    'https://via.placeholder.com/',
+    'https://via.placeholder.com/',
+    'https://via.placeholder.com/',
+    'https://via.placeholder.com/',
+    'https://via.placeholder.com/',
+  ];
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div>
+        <MainSlider images={TEMP_MAIN_IMAGES} />
+      </div>
+      <div>
+        <FoodSlider images={TEMP_FOOD_IMAGES} />
+      </div>
+    </div>
+  );
 }
