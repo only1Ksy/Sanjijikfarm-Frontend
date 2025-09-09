f36b2dc1a3030784fd51ab5ff95c646dde3c2c67
only1Ksy <winterksy@gmail.com>
2025-09-09 15:30:57 +0900

    feat: apply components to localfood page

--- sanjijikfarm/src/pages/LocalfoodPage.jsx
+++ sanjijikfarm/src/pages/LocalfoodPage.jsx
@@ -1,3 +1,140 @@
+import { useCallback, useEffect, useState } from 'react';
+import debounce from 'lodash/debounce';
+
+import FoundNearStoreButton from '@/components/feature/localfood/FoundNearStoreButton';
+import LocalfoodModal from '@/components/feature/localfood/LocalfoodModal';
+import SearchBar from '@/components/feature/localfood/SearchBar';
+
 export default function LocalfoodPage() {
-  return <div>localfood</div>;
+  const [filter, setFilter] = useState('거리순');
+
+  // 입력창에 보여줄 바로 반응하는 상태
+  const [inputValue, setInputValue] = useState('');
+  // 디바운싱 적용할 검색 트리거 상태
+  const [searchKeyword, setSearchKeyword] = useState('');
+  // 디바운싱 적용
+  const debouncedSearch = useCallback(
+    debounce((value) => {
+      setSearchKeyword(value.trim());
+    }, 500), // 500ms
+    [],
+  );
+  // searchKeyword 변경 시마다 API 호출
+  useEffect(() => {
+    if (searchKeyword) {
+      // TODO: API 호출 (filter마다 정렬 다르게)
+      console.log('Searching for:', searchKeyword);
+    }
+  }, [searchKeyword]);
+
+  // input 입력 시 호출됨
+  const handleChange = (value) => {
+    setInputValue(value); // 입력값은 즉시 반영
+    debouncedSearch(value); // 검색 트리거는 디바운스 처리
+  };
+
+  // 엔터 누르면 바로 검색 실행
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      debouncedSearch.cancel(); // 디바운싱 취소
+      setSearchKeyword(inputValue); // 즉시 검색
+    }
+  };
+
+  // 검색 아이콘 클릭 시 검색 실행
+  const handleSearch = () => {
+    debouncedSearch.cancel();
+    setSearchKeyword(inputValue);
+  };
+
+  // 필터 토글
+  const toggleFilter = () => {
+    setFilter((prev) => (prev === '거리순' ? '평점순' : '거리순'));
+  };
+
+  const TEMP_LOCALFOOD_SHOP_LIST = [
+    {
+      name: '김포로컬푸트 공동판매장',
+      rating: 3.8,
+      reviewCount: 37,
+      address: '경기도 김포시',
+      url: 'https://via.placeholder.com/',
+    },
+    {
+      name: '김포로컬푸트 공동판매장',
+      rating: 3.8,
+      reviewCount: 37,
+      address: '경기도 김포시',
+      url: 'https://via.placeholder.com/',
+    },
+    {
+      name: '김포로컬푸트 공동판매장',
+      rating: 3.8,
+      reviewCount: 37,
+      address: '경기도 김포시',
+      url: 'https://via.placeholder.com/',
+    },
+    {
+      name: '김포로컬푸트 공동판매장',
+      rating: 3.8,
+      reviewCount: 37,
+      address: '경기도 김포시',
+      url: 'https://via.placeholder.com/',
+    },
+    {
+      name: '김포로컬푸트 공동판매장',
+      rating: 3.8,
+      reviewCount: 37,
+      address: '경기도 김포시',
+      url: 'https://via.placeholder.com/',
+    },
+    {
+      name: '김포로컬푸트 공동판매장',
+      rating: 3.8,
+      reviewCount: 37,
+      address: '경기도 김포시',
+      url: 'https://via.placeholder.com/',
+    },
+    {
+      name: '김포로컬푸트 공동판매장',
+      rating: 3.8,
+      reviewCount: 37,
+      address: '경기도 김포시',
+      url: 'https://via.placeholder.com/',
+    },
+    {
+      name: '김포로컬푸트 공동판매장',
+      rating: 3.8,
+      reviewCount: 37,
+      address: '경기도 김포시',
+      url: 'https://via.placeholder.com/',
+    },
+    {
+      name: '김포로컬푸트 공동판매장',
+      rating: 3.8,
+      reviewCount: 37,
+      address: '경기도 김포시',
+      url: 'https://via.placeholder.com/',
+    },
+    {
+      name: '김포로컬푸트 공동판매장',
+      rating: 3.8,
+      reviewCount: 37,
+      address: '경기도 김포시',
+      url: 'https://via.placeholder.com/',
+    },
+  ];
+
+  return (
+    <div>
+      <SearchBar
+        keyword={inputValue}
+        handleChange={handleChange}
+        handleKeyDown={handleKeyDown}
+        handleSearch={handleSearch}
+      />
+      <FoundNearStoreButton />
+      <LocalfoodModal shopList={TEMP_LOCALFOOD_SHOP_LIST} filter={filter} toggleFilter={toggleFilter} />
+    </div>
+  );
 }
