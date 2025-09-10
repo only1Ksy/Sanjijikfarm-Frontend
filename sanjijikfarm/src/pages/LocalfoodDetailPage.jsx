83b1d6794c7c90b420c0ed4c34ff97f7920ec2b5
only1Ksy <winterksy@gmail.com>
2025-09-10 20:58:05 +0900

    feat: apply components to localfood detail page

--- sanjijikfarm/src/pages/LocalfoodDetailPage.jsx
+++ sanjijikfarm/src/pages/LocalfoodDetailPage.jsx
@@ -1,3 +1,82 @@
+import { useState } from 'react';
+
+import HeartLocalfood from '@/components/common/heart/HeartLocalfood';
+import Review from '@/components/common/review/Review';
+import LocalfoodDetailToggle from '@/components/feature/localfood/localfood-detail/LocalfoodDetailToggle';
+import UpperLocafoodInfo from '@/components/feature/localfood/localfood-detail/UpperLocafoodInfo';
+
 export default function LocalfoodDetailPage() {
-  return <div>LocalfoodDetailPage</div>;
+  const [activeTab, setActiveTab] = useState('menu');
+
+  const TEMP_LOCALFOOD_DETAIL = {
+    id: 1,
+    name: '김포로컬푸트 공동판매장',
+    rating: 3.8,
+    reviewCount: 37,
+    address: '경기도 김포시',
+    url: 'https://via.placeholder.com/',
+    phone: '010-1234-5678',
+  };
+
+  const TEMP_MENU_LIST = [
+    {
+      id: 1,
+      name: '사과',
+      price: 3000,
+      hasNoReview: false,
+      rating: 4.2,
+      likeCount: 45,
+      url: 'https://via.placeholder.com/150',
+    },
+    {
+      id: 2,
+      name: '배',
+      price: 4000,
+      hasNoReview: false,
+      rating: 4.0,
+      likeCount: 30,
+      url: 'https://via.placeholder.com/150',
+    },
+    {
+      id: 3,
+      name: '감',
+      price: 5000,
+      hasNoReview: false,
+      rating: 3.8,
+      likeCount: 22,
+      url: 'https://via.placeholder.com/150',
+    },
+    {
+      id: 4,
+      name: '귤',
+      price: 6000,
+      hasNoReview: false,
+      rating: 4.5,
+      likeCount: 60,
+      url: 'https://via.placeholder.com/150',
+    },
+    {
+      id: 5,
+      name: '포도',
+      price: 7000,
+      hasNoReview: false,
+      rating: 4.1,
+      likeCount: 38,
+      url: 'https://via.placeholder.com/150',
+    },
+  ];
+
+  const TEMP_REVIEW_LIST = [];
+
+  return (
+    <div className="flex h-full w-full flex-col">
+      <UpperLocafoodInfo shop={TEMP_LOCALFOOD_DETAIL} />
+      <LocalfoodDetailToggle onSelect={setActiveTab} active={activeTab} />
+      <div className="scrollbar-hide flex flex-grow flex-col overflow-scroll">
+        {activeTab === 'menu'
+          ? TEMP_MENU_LIST.map((localfood) => <HeartLocalfood key={localfood.id} localfood={localfood} />)
+          : TEMP_REVIEW_LIST.map((review) => <Review key={review.id} review={review} />)}
+      </div>
+    </div>
+  );
 }
