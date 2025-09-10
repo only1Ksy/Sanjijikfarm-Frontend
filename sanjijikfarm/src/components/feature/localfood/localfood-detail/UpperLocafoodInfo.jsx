e3bc79a670e0ba47f8c807e4fe9cb4dc9c395c53
only1Ksy <winterksy@gmail.com>
2025-09-10 20:57:45 +0900

    feat: add upper localfood information component

---
+++ sanjijikfarm/src/components/feature/localfood/localfood-detail/UpperLocafoodInfo.jsx
@@ -0,0 +1,20 @@
+import StarIcon from '@/assets/icons/star.svg';
+
+export default function UpperLocafoodInfo({ shop }) {
+  return (
+    <div className="flex w-full flex-col gap-1">
+      <div className="h-33 w-full">
+        <img className="bg-pale-green h-full w-full object-cover" src={shop.url} alt="localfood detail img" />
+      </div>
+      <div className="flex w-full flex-col gap-2 p-3">
+        <span className="text-title-3 font-bold">{shop.name}</span>
+        <div className="flex items-center gap-0.5">
+          <StarIcon />
+          <span className="text-main-brown text-body-2-med font-medium">{shop.rating.toFixed(1)}</span>
+        </div>
+        <span className="text-body-2-med text-gray-7 font-medium">주소 | {shop.address}</span>
+        <span className="text-body-2-med text-gray-7 font-medium">전화 | {shop.phone}</span>
+      </div>
+    </div>
+  );
+}
