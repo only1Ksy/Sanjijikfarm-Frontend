6e411de9bd21511dc8ad78eb13f530a5bb9007ee
only1Ksy <winterksy@gmail.com>
2025-09-19 14:21:10 +0000

    fix: layout mobile optimization

--- sanjijikfarm/src/components/common/layout/ProtectedLayout.jsx
+++ sanjijikfarm/src/components/common/layout/ProtectedLayout.jsx
@@ -5,12 +5,14 @@ import NavBar from './NavBar';
 
 export default function ProtectedLayout() {
   return (
-    <div className="relative flex h-screen w-full max-w-[var(--frame-width)] flex-col shadow">
+    <div className="relative flex h-[100dvh] w-full max-w-[var(--frame-width)] flex-col shadow">
       <Header />
       <main className="scrollbar-hide flex-1 overflow-y-auto pt-16 pb-16">
         <Outlet />
       </main>
-      <NavBar />
+      <div className="pb-[env(safe-area-inset-bottom)]">
+        <NavBar />
+      </div>
     </div>
   );
 }
