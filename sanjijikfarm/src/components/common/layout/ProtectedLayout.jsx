9924306f02fc28bda3b983e255297778a392b0d6
only1Ksy <winterksy@gmail.com>
2025-09-19 13:48:55 +0000

    fix: change h-screen to h-100dvh

--- sanjijikfarm/src/components/common/layout/ProtectedLayout.jsx
+++ sanjijikfarm/src/components/common/layout/ProtectedLayout.jsx
@@ -5,7 +5,7 @@ import NavBar from './NavBar';
 
 export default function ProtectedLayout() {
   return (
-    <div className="h-screen w-full">
+    <div className="h-[100dvh] w-full">
       <div className="relative mx-auto h-full max-w-[var(--frame-width)] shadow">
         <Header />
         <main className="scrollbar-hide h-full w-full overflow-scroll pt-16 pb-16">
