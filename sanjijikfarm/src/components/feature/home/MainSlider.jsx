b58ce1743f6cdeedb57f67659b3ed86e31def028
only1Ksy <winterksy@gmail.com>
2025-10-19 13:50:51 +0000

    feat: add mock image for home banner

--- sanjijikfarm/src/components/feature/home/MainSlider.jsx
+++ sanjijikfarm/src/components/feature/home/MainSlider.jsx
@@ -4,7 +4,9 @@ import 'swiper/css/pagination';
 import { Autoplay, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-export default function MainSlider({ images }) {
+import SLIDES from '@/lib/mock/HomeSliderMockData.json';
+
+export default function MainSlider() {
   return (
     <Swiper
       autoplay={{
@@ -13,19 +15,19 @@ export default function MainSlider({ images }) {
       }}
       modules={[Pagination, Autoplay]}
     >
-      {images.map((img, index) => (
+      {SLIDES.map((slide, index) => (
         <SwiperSlide key={index}>
           <div className="bg-pale-green relative h-52 w-full">
             {/* 이미지 */}
-            <img src={img.url} className="h-full w-full object-cover" alt={`Main Slide ${index + 1}`} />
+            <img src={slide.url} className="h-full w-full object-cover" alt={`Main Slide ${index + 1}`} />
 
             {/* 아래 그라데이션 오버레이 */}
             <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-black/20 to-transparent" />
 
             {/* 텍스트 (왼쪽 아래) */}
             <div className="absolute bottom-3 left-4 text-white">
-              <h2 className="text-lg font-bold">Main Title {index + 1}</h2>
-              <p className="text-sm opacity-80">Sub Title goes here</p>
+              <h2 className="text-lg font-bold">{slide.title}</h2>
+              <p className="text-sm opacity-80">{slide.subtitle}</p>
             </div>
           </div>
         </SwiperSlide>
--- sanjijikfarm/src/pages/HomePage.jsx
+++ sanjijikfarm/src/pages/HomePage.jsx
@@ -3,18 +3,12 @@ import MainDecreaseAmount from '@/components/feature/home/MainDecreaseAmount';
 import MainSlider from '@/components/feature/home/MainSlider';
 
 export default function HomePage() {
-  const TEMP_MAIN_IMAGES = [
-    { url: 'https://via.placeholder.com/600x300', main: 'Main Title', sub: 'Sub Title goes here' },
-    { url: 'https://via.placeholder.com/600x300', main: 'Main Title', sub: 'Sub Title goes here' },
-    { url: 'https://via.placeholder.com/600x300', main: 'Main Title', sub: 'Sub Title goes here' },
-  ];
-
   const month = new Date().getMonth();
 
   return (
     <div className="flex flex-col gap-10">
       <div>
-        <MainSlider images={TEMP_MAIN_IMAGES} />
+        <MainSlider />
       </div>
       <div className="flex flex-col items-center gap-4">
         <span className="text-heading-1 font-bold">{month + 1}월의 식재료 추천</span>
